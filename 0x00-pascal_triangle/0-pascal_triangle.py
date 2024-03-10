644af47ba840eea05c5749b88cd9794e9542e82c
gladysdo <doe.glad@gmail.com>
2024-03-10 21:54:10 +0000

    Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,26 @@
+#!/usr/bin/python3
+"""
+Take a number of rows to be printed, lets assume it to be n
+Make outer iteration i from 0 to n times to print the rows.
+Make inner iteration for j from 0 to (N – 1).
+Print single blank space ” “.
+Close inner loop (j loop) //its needed for left spacing.
+Make inner iteration for j from 0 to i.
+Print nCr of i and j.
+Close inner loop.
+Print newline character () after each inner iteration
+"""
+
 def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1] * (i + 1) for i in range(n)]
-
-    for i in range(2, n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
         for j in range(1, i):
-            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
-
-def print_triangle(triangle):
-    for row in triangle:
-        print("[{}]".format(",".join(map(str, row))))
-
-if __name__ == "__main__":
-    n = 5
-    triangle = pascal_triangle(n)
-    print_triangle(triangle)
